2fb357e8cb4ffe2b01482bbbe837c7c360c9b877
Eva <as45986@gmail.com>
2023-07-02 14:25:48 +0800

    fix(vite-config):fix set config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-UI-Component-v1/',
+  base: '/vite-eCommerce/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
